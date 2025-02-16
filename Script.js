bc647d72ad9a51385882137a35ac1ee8ba05cd1a
Bradley-assignment2 <50049828@belfastmet.ac.uk>
2025-02-16 18:30:27 +0000

    finished accordion menus in courses page

--- Script.js
+++ Script.js
@@ -1,2 +1,20 @@
 /* This is where we will add any custom JavaScript features*/
 console.log("Welcome to the Belfast Met Access Site");
+
+/*This function allows the use of accordion menus on the site*/
+var accordions =
+document.getElementsByClassName("accordion");
+
+for (var i = 0; i < accordions.length; i++) {
+  accordions[i].onclick = function() {
+    this.classList.toggle('is-open');
+    var content = this.nextElementSibling;
+
+    if (content.style.maxHeight)  {
+       
+        content.style.maxHeight = null;
+    } else {
+        content.style.maxHeight = content.scrollHeight + "px";
+    }
+  }
+}
